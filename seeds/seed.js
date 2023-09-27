6ca3f732c5dcda4bc1bbfafba1543d53eab2e08d
Tim Morgan <132379127+tmorgan-dev@users.noreply.github.com>
2023-09-27 17:02:28 -0600

    added logic to seeds/seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,24 +1,33 @@
 const sequelize = require('../config/connection');
-const { Labels, Sets, Wishlist } = require('../models');
+const { Labels, Sets, Wishlist, User } = require('../models');
 
 const labelData = require('./labelData.json');
 const setData = require('./setData.json');
 const wishlistData = require('./wishlistData.json');
+const userData = require('./userData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const users = await Labels.bulkCreate(labelData, {
+  const users = await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+  const labels = await Labels.bulkCreate(labelData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const sets = await Sets.bulkCreate(setData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const wishlist = await Wishlist.bulkCreate(wishlistData, {
+    individualHooks: true,
+    returning: true,
+  });
 
   process.exit(0);
 };
