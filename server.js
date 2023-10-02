a72c742010d964afa7553b9d1d88a3f79bbf7360
Tim Morgan <132379127+tmorgan-dev@users.noreply.github.com>
2023-10-02 11:25:15 -0600

    updated logic in routes, handlebars and sever.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.set('views', './views');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static("images"));
 
 
 // turn on routes
