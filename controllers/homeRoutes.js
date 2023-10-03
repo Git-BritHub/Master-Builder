4859986fc49917ed52861dc8d5b30b148adeb1a3
Tim Morgan <132379127+tmorgan-dev@users.noreply.github.com>
2023-10-02 22:00:36 -0600

    added logic to homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 // TODO: 'Collection' in place of project
 const { User } = require('../models');
-// const withAuth = require('../utils/auth');
+const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
   try {
@@ -50,24 +50,25 @@ router.get('/', (req, res) => {
 // });
 
 // Use withAuth middleware to prevent access to route
-// router.get('/profile', withAuth, async (req, res) => {
-//   try {
-//     // Find the logged in user based on the session ID
-//     const userData = await User.findByPk(req.session.user_id, {
-//       attributes: { exclude: ['password'] },
-//       include: [{ model: Project }],
-//     });
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // TODO: Fix the data request to get data from the DB in the proper format
+    // Find the logged in user based on the session ID
+    // const userData = await User.findByPk(req.session.user_id, {
+    //   attributes: { exclude: ['password'] },
+    //   include: [{ model: Project }],
+    // });
 
-//     const user = userData.get({ plain: true });
+    // const user = userData.get({ plain: true });
 
-//     res.render('profile', {
-//       ...user,
-//       logged_in: true
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
+    res.render('profile', {
+      // ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
